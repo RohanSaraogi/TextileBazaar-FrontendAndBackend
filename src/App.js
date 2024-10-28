3209d5893c57ad4ef47a6340a5d073b3b5455058
RohanSaraogi <rohan.ce20@sot.pdpu.ac.in>
2024-10-28 10:35:52 +0530

    vercel page speed insights

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import {
